589ceba939ced442c4388799c32d7651d0a41976
dhiraj jadhav <dhiraj.jadhav20@pccoepune.org>
2024-03-12 20:02:00 +0530

    with dashboard

---
+++ lib/features/user_auth/presentation/pages/dashboard.dart
@@ -0,0 +1,137 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../global/common/toast.dart';
+
+class DashBoard extends StatelessWidget {
+  const DashBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          margin: EdgeInsets.only(top: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  FirebaseAuth.instance.signOut();
+                  Navigator.pushNamed(context, "/login");
+                  showToast(message: "Successfully signed out");
+                },
+                child: Container(
+                  height: 45,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Center(
+                    child: Text(
+                      "Sign out",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Container(
+        color: const Color(0xFFCFF3F8),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 150.0,
+                      height: 210.0,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'assets/image/insti.png',
+                              fit: BoxFit.contain,
+                            ),
+                            const Text(
+                              'Organisation ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 150.0,
+                      height: 210.0,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'assets/image/cont.png',
+                              fit: BoxFit.cover,
+                            ),
+                            const Text(
+                              'Make Request',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 150.0,
+                      height: 210.0,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'assets/image/donate.png',
+                              fit: BoxFit.cover,
+                            ),
+                            const Text(
+                              'Donate Blood',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
